34e0072229f22fb0426a3461ef8203ec641050d9
camiblanch <caminblanch@gmail.com>
2020-12-01 20:16:16 -0700

    preps day 01, part 02

--- day01/main.js
+++ day01/main.js
@@ -5,12 +5,20 @@ const readFile = (file = "input.txt") => {
 
 const entries = readFile();
 
-loop1:
-	for (let i = 0; i < entries.length; i++) {
-		for (let j = i + 1; j < entries.length; j++) {
-			if (entries[i] + entries[j] === 2020) {
-				console.log(entries[i] * entries[j]);
-				break loop1;
+const part1 = () => {
+	loop1:
+		for (let i = 0; i < entries.length; i++) {
+			for (let j = i + 1; j < entries.length; j++) {
+				if (entries[i] + entries[j] === 2020) {
+					console.log(entries[i] * entries[j]);
+					break loop1;
+				}
 			}
 		}
-	}
+};
+
+const part2 = () => {
+
+}
+
+part2();
