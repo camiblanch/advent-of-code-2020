5edc0910558a7b83d9b51366d9a301982b067bca
camiblanch <caminblanch@gmail.com>
2020-12-03 20:13:11 -0700

    day 2 part 1

--- day02/main.js
+++ day02/main.js
@@ -1,4 +1,20 @@
 const readFile = (file = "input.txt") => {
 	const fs = require("fs");
-	return fs.readFileSync(file).toString().split("\n").map(Number);
+	return fs.readFileSync(file).toString().split("\n");
 };
+
+const passwordEntries = readFile();
+
+const validPasswords = passwordEntries.filter(passwordEntry => {
+	const [passwordRule, password] = passwordEntry.split(": ");
+	const [characterRange, character] = passwordRule.split(" ");
+	const [min, max] = characterRange.split("-").map(Number);
+	// {min, max, character, password};
+	const numTimesCharOccurs = password.split("").filter(passwordCharacter => (passwordCharacter === character)).length;
+
+	if (numTimesCharOccurs >= min && numTimesCharOccurs <= max) {
+		return true;
+	}
+});
+
+console.log(validPasswords.length);
