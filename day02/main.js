1d961e75566fc599aa904a59616661f9a19e968f
camiblanch <caminblanch@gmail.com>
2020-12-03 20:21:27 -0700

    day 2 part 2

--- day02/main.js
+++ day02/main.js
@@ -5,15 +5,37 @@ const readFile = (file = "input.txt") => {
 
 const passwordEntries = readFile();
 
-const validPasswords = passwordEntries.filter(passwordEntry => {
-	const [passwordRule, password] = passwordEntry.split(": ");
-	const [characterRange, character] = passwordRule.split(" ");
-	const [min, max] = characterRange.split("-").map(Number);
-	const numTimesCharOccurs = password.split("").filter(passwordCharacter => (passwordCharacter === character)).length;
+const part1 = () => {
+	const validPasswords = passwordEntries.filter(passwordEntry => {
+		const [passwordRule, password] = passwordEntry.split(": ");
+		const [characterRange, character] = passwordRule.split(" ");
+		const [min, max] = characterRange.split("-").map(Number);
+		const numTimesCharOccurs = password.split("").filter(passwordCharacter => (passwordCharacter === character)).length;
 
-	if (numTimesCharOccurs >= min && numTimesCharOccurs <= max) {
-		return true;
-	}
-});
+		if (numTimesCharOccurs >= min && numTimesCharOccurs <= max) {
+			return true;
+		}
+	});
 
-console.log(validPasswords.length);
+	console.log(validPasswords.length);
+};
+
+const part2 = () => {
+	const validPasswords = passwordEntries.filter(passwordEntry => {
+		const [passwordRule, password] = passwordEntry.split(": ");
+		const [characterRange, character] = passwordRule.split(" ");
+		const [position1, position2] = characterRange.split("-").map(Number);
+		const passwordAsArray = password.split("");
+		const position1Char = passwordAsArray[position1 - 1];
+		const position2Char = passwordAsArray[position2 - 1];
+
+		if ((position1Char === character && position2Char !== character)
+			|| (position1Char !== character && position2Char === character)) {
+			return true;
+		}
+	});
+
+	console.log(validPasswords.length);
+};
+
+part2();
