dac3134af8e90c9434172e5f85b8580deba0e234
camiblanch <caminblanch@gmail.com>
2020-12-05 23:45:52 -0700

    reads file properly, sample input

--- day06/main.js
+++ day06/main.js
@@ -1,11 +1,12 @@
 const readFile = (file = "input.txt") => {
 	const fs = require("fs");
-	return fs.readFileSync(file).toString().split("\n\n");
+	return fs.readFileSync(file).toString().split("\n\n").map(group=>group.split("\n"));
 };
 
 const part1 = () => {
-
+	console.log(readFile());
 };
+
 const part2 = () => {
 
 };
